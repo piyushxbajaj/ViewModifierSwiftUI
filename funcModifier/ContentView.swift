ee385f9d966272ec1f1e1ef15c6df6fd62188acc
piyushxbajaj <piyushpiyush41@gmail.com>
2020-04-04 14:04:45 +0530

    Initial Commit

---
+++ funcModifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  funcModifier
+//
+//  Created by Piyush Bajaj on 04/04/20.
+//  Copyright © 2020 Piyush Bajaj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
